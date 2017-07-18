1ec782cfd0461c5c351f43d2772b9b8a86b4ba10
Nikolay Zyuz <n.zyuz@itransition.com>
2017-07-18 17:03:32 +0300

    Code for version 0.0.2

---
+++ index.js
@@ -0,0 +1,21 @@
+function initCounter() {
+    window.activeFetchCalls = 0;
+
+    const done = function() {
+        window.activeFetchCalls--;
+    };
+
+    if (window.fetch) {
+        debugger;
+        const originalFetch = window.fetch;
+
+        window.fetch = function(url, options) {
+            const originalFetchPromise = originalFetch(url, options);
+            window.activeFetchCalls--;
+            originalFetchPromise.then(done, done);
+            return originalFetchPromise;
+        }
+    }
+}
+
+module.exports = initCounter;
